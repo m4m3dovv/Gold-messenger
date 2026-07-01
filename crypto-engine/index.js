80cb764d11fe800e5ca1cac2f86c1a5908ee6864
m4m3dovv <goldkassa@atomicmail.io>
2026-07-01 23:25:54 +0400

    Delete crypto-engine directory

--- crypto-engine/index.js
+++
@@ -1,3 +0,0 @@
-export { encryptMessage, decryptMessage, HEADER_LEN } from './src/pipeline.js';
-export { generateKeyPair, publicKeyFromPrivate, computeSharedSecret, conversationSalt } from './src/identity.js';
-export { deriveLayerKeys } from './src/keys.js';
